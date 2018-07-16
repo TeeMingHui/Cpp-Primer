fbfff525df9566b4679982e8895ff052bae5ae7d
Ming hui <minghui@ecosensolutions.com>
2018-07-16 12:50:40 +0800

    Update ex6_21.cpp
    added notes

--- ch06/ex6_21.cpp
+++ ch06/ex6_21.cpp
@@ -1,23 +1,29 @@
-// @Yue Wang
-//
-// Exercise 6.21:
-// Write a function that takes an int and a pointer to an int and
-// returns the larger of the int value or the value to which the
-// pointer points. What type should you use for the pointer?
-//
-
 #include <iostream>
-using std::cout;
+#include <cstring>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <stdexcept>
 
-int larger_one(const int i, const int *const p)
+using std::begin; using std::end; using std::cout; using std::endl; using std::vector;
+using std::string; using std::cin; using std::pair; 
+
+ 
+// For Pointer, it "Points" to the address. With that, have to provide it address! They come in pair, so int *p = &I
+// The other side need to follow the convention. 
+
+int larger(const int lhs, const int *const rhs)
 {
-    return (i > *p) ? i : *p;
+	return ( ( lhs > *rhs) ? lhs : *rhs);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-    int i = 6;
-    cout << larger_one(7, &i);
+	int value = 9;
+	const int p = 9;
+	cout << "P without *" << p << endl;
+	cout << "90 vs P: " << larger(90, &p) << endl;
+	cout << "01 vs P: " << larger(1, &p) << endl;
 
-    return 0;
+	return 0;
 }
