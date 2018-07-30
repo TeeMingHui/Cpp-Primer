2ace34819b66baa177ecaab9297c316c7aaec68b
Ming hui <minghui@ecosensolutions.com>
2018-07-30 10:04:00 +0800

    Update ex6_23.cpp

--- ch06/ex6_23.cpp
+++ ch06/ex6_23.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
-using std::cout; using std::endl; using std::begin; using std::end;
 
-void print(const int *pi)
+void print(const char* lhs)
 {
-    if(pi)
-        cout << *pi << endl;
+	std::cout << "Function 1" << std::endl;
+	if(lhs)
+		while(*lhs)
+			std::cout << *lhs++ << std::endl;
 }
 
-void print(const char *p)
+void print(const int* lhs)
 {
-    if (p)
-        while (*p) cout << *p++;
-    cout << endl;
+	std::cout << "Function 2" << std::endl;
+	if(lhs)
+		while(*lhs)
+			std::cout << *lhs++ << std::endl;
 }
 
 void print(const int *beg, const int *end)
 {
-    while (beg != end)
-        cout << *beg++ << endl;
+	std::cout << "Function 3" << std::endl;
+	while(beg != end)
+		std::cout << *beg++ << std::endl;
 }
 
-void print(const int ia[], size_t size)
+void print(const int arr[], size_t size)
 {
-    for (size_t i = 0; i != size; ++i) {
-        cout << ia[i] << endl;
-    }
+	std::cout << "Function 4" << std::endl;
+	for(size_t i = 0; i != size; ++i)
+		std::cout << arr[i] << std::endl;
 }
 
-void print(int (&arr)[2])
+void print(const int (&arr)[2])
 {
-    for (auto i : arr)
-        cout << i << endl;
+	std::cout << "Function 5" << std::endl;
+	for(auto elem : arr)
+		std::cout << elem << std::endl;
 }
 
-int main()
+
+int main(int argc, char const *argv[])
 {
-    int i = 0, j[2] = { 0, 1 };
+	int i = 0, j[2] = { 0, 1 };
     char ch[5] = "pezy";
-    
     print(ch);
-    print(begin(j), end(j));
     print(&i);
-    print(j, end(j)-begin(j));
+    print(std::begin(j), std::end(j));
+    print(j,(std::end(j)-std::begin(j)));
     print(j);
-    
-    return 0;
+	return 0;
 }
